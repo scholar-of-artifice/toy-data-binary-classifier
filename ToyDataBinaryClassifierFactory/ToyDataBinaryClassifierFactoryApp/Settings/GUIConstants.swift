9fdecfd9f9fd8b2cdd509daa5a12ab0744034a31
scholar-of-artifice <206404745+scholar-of-artifice@users.noreply.github.com>
2026-09-14 22:41:26 +0100

    Create GUIConstants.swift
    Create a basic set of variables to keep GUI styles consistent.

---
+++ ToyDataBinaryClassifierFactory/ToyDataBinaryClassifierFactoryApp/Settings/GUIConstants.swift
@@ -0,0 +1,18 @@
+//
+//  GUIConstants.swift
+//  ToyDataBinaryClassifierFactory
+//
+//  Created by scholar-of-artifice on 14/09/2026.
+//
+
+import SwiftUI
+
+struct GUIConstants {
+    static let PADDING: CGFloat = 8.0
+    static let SPACING: CGFloat = 16.0
+    static let RADIUS: CGFloat = 8.0
+    static let OPACITY: CGFloat = 0.5
+    static let SHADOW_X: CGFloat = 1
+    static let SHADOW_Y: CGFloat = 2
+    static let LINE_WIDTH: CGFloat = 1
+}
