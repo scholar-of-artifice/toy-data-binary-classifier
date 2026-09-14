4a68e5ed75b12ab8bc9f8b5c1a44c871c4be3636
scholar-of-artifice <206404745+scholar-of-artifice@users.noreply.github.com>
2026-09-14 22:43:34 +0100

    Update ContentView.swift
    Use the CoreConfiguration view. remove boilerplate.

--- ToyDataBinaryClassifierFactory/ToyDataBinaryClassifierFactoryApp/ContentView.swift
+++ ToyDataBinaryClassifierFactory/ToyDataBinaryClassifierFactoryApp/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            RecessedContainer(
+                title: "Core Configuration",
+                content: CoreConfiguration()
+            )
         }
         .padding()
     }
