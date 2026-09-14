f5dba0babacdac575792668ff39112a89b2681ae
scholar-of-artifice <206404745+scholar-of-artifice@users.noreply.github.com>
2026-09-14 21:03:54 +0100

    New Target
    Create a macOS target App to use SwiftUI

---
+++ ToyDataBinaryClassifierFactory/ToyDataBinaryClassifierFactoryAppTests/ToyDataBinaryClassifierFactoryAppTests.swift
@@ -0,0 +1,19 @@
+//
+//  ToyDataBinaryClassifierFactoryAppTests.swift
+//  ToyDataBinaryClassifierFactoryAppTests
+//
+//  Created by scholar-of-artifice on 14/09/2026.
+//
+
+import Testing
+@testable import ToyDataBinaryClassifierFactoryApp
+
+struct ToyDataBinaryClassifierFactoryAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
