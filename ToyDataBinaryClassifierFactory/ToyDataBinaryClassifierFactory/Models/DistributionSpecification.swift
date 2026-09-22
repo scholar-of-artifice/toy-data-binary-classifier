89c22a7402ccf401101c1e5ecdf230feb210df7a
scholar-of-artifice <206404745+scholar-of-artifice@users.noreply.github.com>
2026-09-22 23:52:38 +0100

    Update DistributionSpecification.swift
    make DistributionSpecification serializable to json

--- ToyDataBinaryClassifierFactory/ToyDataBinaryClassifierFactory/Models/DistributionSpecification.swift
+++ ToyDataBinaryClassifierFactory/ToyDataBinaryClassifierFactory/Models/DistributionSpecification.swift
@@ -8,10 +8,34 @@
 import Foundation
 
 /// Represents a distribution and its validated parameters
-enum DistributionSpecification {
+enum DistributionSpecification: Encodable {
 
     case uniform(min: Float, max: Float)
     case normal(mu: Float, sigma: Float)
+
+    // keys used inside the nested "parameters" object
+    private enum UniformCodingKeys: String, CodingKey {
+        case minimum
+        case maximum
+    }
+    private enum NormalCodingKeys: String, CodingKey {
+        case mu
+        case sigma
+    }
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: DynamicCodingKey.self)
+
+        switch self {
+        case .uniform(let min, let max):
+            var nested = encoder.container(keyedBy: UniformCodingKeys.self)
+            try nested.encode(min, forKey: .minimum)
+            try nested.encode(max, forKey: .maximum)
+        case .normal(let mu, let sigma):
+            var nested = encoder.container(keyedBy: NormalCodingKeys.self)
+            try nested.encode(mu, forKey: .mu)
+            try nested.encode(sigma, forKey: .sigma)
+        }
+    }
 
     /// Factory method for uniform distribution with validation
     static func makeUniform(min: Float, max: Float) throws
@@ -33,3 +57,18 @@ enum DistributionSpecification {
     }
 }
 
+private struct DynamicCodingKey: CodingKey {
+
+    var stringValue: String
+    var intValue: Int?
+
+    init?(stringValue: String) {
+        self.stringValue = stringValue
+        self.intValue = nil
+    }
+    
+    init?(intValue: Int) {
+        self.stringValue = "\(intValue)"
+        self.intValue = intValue
+    }
+}
