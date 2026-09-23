72c5057ddcc8e30ea76e5af8a1f2edf21a665ced
scholar-of-artifice <206404745+scholar-of-artifice@users.noreply.github.com>
2026-09-23 22:59:27 +0100

    Update DistributionSpecificationTests.swift
    complete tests to catch errors

--- ToyDataBinaryClassifierFactory/ToyDataBinaryClassifierFactoryTests/Models/DistributionSpecificationTests.swift
+++ ToyDataBinaryClassifierFactory/ToyDataBinaryClassifierFactoryTests/Models/DistributionSpecificationTests.swift
@@ -17,7 +17,7 @@ struct DistributionSpecificationTests {
     @Test("Successful Uniform Initialization Test")
     func successfulUniformInitialization() {
         do {
-            let spec = try DistributionSpecification.makeUniform(min: -1.0, max: 1.0)
+            _ = try DistributionSpecification.makeUniform(min: -1.0, max: 1.0)
         } catch {
             Issue.record(
                 "Valid initialization should not throw; received error instead: \(error)"
@@ -28,11 +28,47 @@ struct DistributionSpecificationTests {
     @Test("Successful Normal Initialization Test")
     func successfulNormalInitialization() {
         do {
-            let spec = try DistributionSpecification.makeNormal( mu: 0.0, sigma: 1.0)
+            _ = try DistributionSpecification.makeNormal( mu: 0.0, sigma: 1.0)
         } catch {
             Issue.record(
                 "Valid initialization should not throw; received error instead: \(error)"
             )
+        }
+    }
+    
+    @Test("Failure Normal Initialization Sigma Is Negative")
+    func failsNormalInitializationSigmaIsNegative() {
+        #expect(
+            throws: DistributionError.invalidStandardDeviation(-1.0)
+        ) {
+            try DistributionSpecification.makeNormal( mu: 0.0, sigma: -1.0)
+        }
+    }
+    
+    @Test("Failure Normal Initialization Sigma Is Zero")
+    func failsNormalInitializationSigmaIsZero() {
+        #expect(
+            throws: DistributionError.invalidStandardDeviation(0.0)
+        ) {
+            try DistributionSpecification.makeNormal( mu: 0.0, sigma: 0.0)
+        }
+    }
+    
+    @Test("Failure Uniform Initialization Min == Max")
+    func failsUnifornInitializationMinIsMax() {
+        #expect(
+            throws: DistributionError.invalidRange(min: 1.0, max: 1.0)
+        ) {
+            try DistributionSpecification.makeUniform(min: 1.0, max: 1.0)
+        }
+    }
+    
+    @Test("Failure Uniform Initialization Min > Max")
+    func failsUnifornInitializationMinGreaterThanMax() {
+        #expect(
+            throws: DistributionError.invalidRange(min: 1.0, max: -11.0)
+        ) {
+            try DistributionSpecification.makeUniform(min: 1.0, max: -11.0)
         }
     }
 }
